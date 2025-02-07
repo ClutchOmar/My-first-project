add3918c253b9e08c066b4884a39849396a6633e
ClutchOmar <alshamiomar2004@gmail.com>
2025-02-06 22:45:39 -0500

    Testing
    testing github

---
+++ Assignment1_OmarAlshami.cpp
@@ -0,0 +1,20 @@
+/*==========================================================
+
+ Title:       Assignment1 - First C++ Program
+ Course:      CSC 1100
+ Author:      Omar Alshami
+ Date:       02/06/2025
+ Description: this program prints Hello world!", it also prints prints the sum of 100 + 1.
+
+ ==========================================================
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  //write your code here
+  cout << "Hello World! " << endl; //print Hello World!
+  cout << 100 +1 << endl;   //print the sum of 100 + 1
+  return 0;
+}
